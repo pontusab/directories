4fb2f4ac4635bba890f133b78b12c6833525b877
atezer <atezerdesign@gmail.com>
2025-03-17 11:33:00 +0300

    Add Figma Plugin to Monorepo

---
+++ apps/figma-plugin/webpack.config.js
@@ -0,0 +1,21 @@
+const path = require('path');
+
+module.exports = {
+  entry: './src/code.ts',
+  output: {
+    filename: 'code.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: 'ts-loader',
+        exclude: /node_modules/,
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['.tsx', '.ts', '.js'],
+  },
+}; 
