da08a3283924c6d8b88a389e57997881fbc40e31
vc-sahil <sahil.darji@viitor.cloud>
2025-05-21 14:00:13 +0530

    Add php7.3 web development & api rule

--- packages/data/src/rules/index.ts
+++ packages/data/src/rules/index.ts
@@ -74,6 +74,7 @@ import { webDevelopmentRules } from "./web-development";
 import { webScrapingRules } from "./web-scraping";
 import { wordpressRules } from "./wordpress";
 import { wordpressWoocommerce } from "./wordpress-woocommerce";
+import { phpRules } from "./php";
 
 export const rules: Rule[] = [
   ...ALRules,
@@ -151,6 +152,7 @@ export const rules: Rule[] = [
   ...wordpressWoocommerce,
   ...typescriptRules,
   ...viewComfyRules,
+  ...phpRules,
 ].map(
   (rule): Rule => ({
     ...rule,
