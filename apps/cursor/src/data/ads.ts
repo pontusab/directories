c6cf4fd28c3afee5c23eaad27a1df3143d71baa3
Pontus Abrahamsson <pontus@lostisland.co>
2025-12-21 20:08:22 +0100

    Update next

--- apps/cursor/src/data/ads.ts
+++ apps/cursor/src/data/ads.ts
@@ -9,16 +9,6 @@ export interface Ad {
 
 export const ads: Ad[] = [
   {
-    id: "polar",
-    title: "Polar",
-    description: "The fastest growing engine for SaaS & Digital Products",
-    logoUrl:
-      "https://pub-abe1cd4008f5412abb77357f87d7d7bb.r2.dev/ads-polar-logo.svg",
-    imageUrl:
-      "https://pub-abe1cd4008f5412abb77357f87d7d7bb.r2.dev/ads-polar.png",
-    link: "https://go.midday.ai/JYZ5WcG",
-  },
-  {
     id: "sentry",
     title: "Sentry.io - Build with AI, debug broken code.",
     description:
